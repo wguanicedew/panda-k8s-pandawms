32f03ae36b5422453d114572711abf573c45256d
tmaeno <tmaeno@bnl.gov>
2022-06-26 14:48:49 +0200

    msgsvc

--- helm/msgsvc/charts/activemq/sandbox/install_conf.py
+++ helm/msgsvc/charts/activemq/sandbox/install_conf.py
@@ -25,15 +25,17 @@ auth_entries = """
 u_file.write('system={}\n'.format(basePasswd))
 g_file.write('admins=system\n')
 
-# define R/W users per channel
+# define read/write/owner users per channel
 for channel in channels:
+    u_file.write('{0}={1}_{0}\n'.format(channel, basePasswd))
     u_file.write('{0}_w={1}_{0}_w\n'.format(channel, basePasswd))
     u_file.write('{0}_r={1}_{0}_r\n'.format(channel, basePasswd))
 
-    g_file.write('{0}_w=system,{0}_w\n'.format(channel))
-    g_file.write('{0}_r=system,{0}_w,{0}_r\n'.format(channel))
+    g_file.write('{0}=system,{0}\n'.format(channel))
+    g_file.write('{0}_w=system,{0},{0}_w\n'.format(channel))
+    g_file.write('{0}_r=system,{0},{0}_w,{0}_r\n'.format(channel))
 
-    auth_entries += """<authorizationEntry topic="{0}.>" read="{0}_r" write="{0}_w" admin="{0}_w,admins" />\n""".\
+    auth_entries += """<authorizationEntry topic="{0}.>" read="{0}_r" write="{0}_w" admin="{0},admins" />\n""".\
         format(channel)
 
 # activemq.xml
