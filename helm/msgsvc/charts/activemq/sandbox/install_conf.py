b83548cf4badb13db3c45d99ae2b24f9dbe6bad7
tmaeno <tmaeno@bnl.gov>
2022-06-26 14:45:51 +0200

    msgsvc

--- helm/msgsvc/charts/activemq/sandbox/install_conf.py
+++ helm/msgsvc/charts/activemq/sandbox/install_conf.py
@@ -3,14 +3,16 @@
 import os
 import re
 
-
-
 dstPath = os.environ['ACTIVEMQ_HOME']
-basePasswd = os.environ['ACTIVEMQ_BASE_PASSWD']
 
+# base password
+basePasswd = os.environ.get('ACTIVEMQ_BASE_PASSWD', 'password')
+
+# channels
 channels = os.environ.get('ACTIVEMQ_CHANNELS', '').split()
 channels = [i for i in channels if i]
 
+# properties files
 u_file = open(os.path.join(dstPath, 'users.properties'), 'w')
 g_file = open(os.path.join(dstPath, 'groups.properties'), 'w')
 
@@ -23,7 +25,7 @@ auth_entries = """
 u_file.write('system={}\n'.format(basePasswd))
 g_file.write('admins=system\n')
 
-# R/W users per channel
+# define R/W users per channel
 for channel in channels:
     u_file.write('{0}_w={1}_{0}_w\n'.format(channel, basePasswd))
     u_file.write('{0}_r={1}_{0}_r\n'.format(channel, basePasswd))
@@ -34,11 +36,13 @@ for channel in channels:
     auth_entries += """<authorizationEntry topic="{0}.>" read="{0}_r" write="{0}_w" admin="{0}_w,admins" />\n""".\
         format(channel)
 
+# activemq.xml
 script_dir = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(script_dir, 'activemq.xml')) as src_f:
     activemq_xml = src_f.read()
-    m = re.search(r'(/s+)___AUTH_ENTRIES___', activemq_xml)
-    auth_entries = auth_entries.replace('\n', '\n' + m.group(1)).replace('___AUTH_ENTRIES___', activemq_xml)
+    m = re.search(r'(\s+)___AUTH_ENTRIES___', activemq_xml)
+    if m:
+        auth_entries = auth_entries.replace('\n', '\n' + m.group(1)).replace('___AUTH_ENTRIES___', activemq_xml)
     with open(os.path.join(dstPath, 'activemq.xml'), 'w') as dst_f:
         dst_f.write(activemq_xml)
 
