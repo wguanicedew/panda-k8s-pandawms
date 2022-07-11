a3b25b8c13158d48a1a2ffda34ab841727aaf03a
tmaeno <tmaeno@bnl.gov>
2022-07-11 17:09:26 +0200

    process_template

---
+++ helm/panda/sandbox/process_template.py
@@ -0,0 +1,24 @@
+import os
+import re
+import glob
+
+# loop over all template files
+sandbox_dir = os.path.abspath(os.path.dirname(__file__))
+for name in glob.glob(os.path.join(sandbox_dir, '*.template')):
+    with open(name) as f:
+        template = f.read()
+        # replace placeholders with env vars
+        items = re.findall(r'${(\w+)\}', template)
+        done_list = set()
+        for item in items:
+            if item in done_list:
+                continue
+            done_list.add(item)
+            if item in os.environ:
+                template = template.replace('${'+item+'}', os.environ[item])
+    # dump
+    new_filename = re.sub('\.template$', '', name)
+    if not os.path.exists(new_filename):
+        with open(new_filename, 'w') as f:
+            f.write(template)
+
