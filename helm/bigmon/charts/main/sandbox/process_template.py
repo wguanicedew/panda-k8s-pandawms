41542f625925f56b67214b3f8d8e8e478ff7c7e3
tmaeno <tmaeno@bnl.gov>
2022-07-15 09:39:55 +0200

    bigmon

--- helm/bigmon/charts/main/sandbox/process_template.py
+++ helm/bigmon/charts/main/sandbox/process_template.py
@@ -6,9 +6,6 @@ import glob
 sandbox_dir = os.path.abspath(os.path.dirname(__file__))
 for name in glob.glob(os.path.join(sandbox_dir, '*.template')):
     new_filename = re.sub('\.template$', '', name)
-    # skip already exists
-    if os.path.exists(new_filename):
-        continue
     # patch
     with open(name) as f:
         template = f.read()
