79f0949ea8f7b8fca11faa4ee37f0ecfc1dfe6e4
Wen Guan <wguan.icedew@gmail.com>
2024-03-17 18:03:39 +0100

    dev panda with srun for multi-cores

---
+++ helm/harvester/charts/harvester/sandbox/lsst.rubin-srun.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+### --ntasks-total=8 --ntasks=1 --cpus-per-task=1 --mem-per-cpu=4000
+
+ntasks_total=1
+ntasks=-1
+cpus_per_task=-1
+mem_per_cpu=-1
+
+myargs="$@"
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+case $key in
+    --ntasks-total)
+    ntasks_total="$2"
+    shift
+    shift
+    ;;
+    --ntasks)
+    ntasks="$2"
+    shift
+    shift
+    ;;
+    --cpus-per-task)
+    cpus_per_task="$2"
+    shift
+    shift
+    ;;
+    --mem-per-cpu)
+    mem_per_cpu="$2"
+    shift
+    shift
+    ;;
+    *)
+    POSITIONAL+=("$1") # save it in an array for later
+    shift
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+pilotargs="$@"
+
+cmd="srun"
+if [[ $ntasks -gt 0 ]]; then
+    cmd="$cmd --ntasks $ntasks"
+fi
+if [[ ${cpus_per_task} -gt 0 ]]; then
+    cmd="$cmd --cpus-per-task ${cpus_per_task}"
+fi
+if [[ ${mem_per_cpu} -gt 0 ]]; then
+    cmd="$cmd --mem-per-cpu ${mem_per_cpu}"
+fi
+
+
+export RUCIO_CONFIG=/cvmfs/sw.lsst.eu/linux-x86_64/panda_env/v1.0.9/conda/install/envs/pilot/etc/rucio.cfg.atlas.client.template
+
+# env
+
+echo
+cmd="$cmd --export=ALL /cvmfs/sw.lsst.eu/linux-x86_64/panda_env/v1.0.9/pilot/wrapper/rubin-wrapper.sh $@"
+echo $cmd
+
+ntasks=${ntasks_total}
+for i in $(seq 1 $ntasks); do
+    $cmd | sed -e "s/^/pilot_$i: /"  &
+done
+
+wait
